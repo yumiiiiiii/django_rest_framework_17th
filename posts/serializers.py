5f3a90fd3c2e5e276f47785d7a76cebaa9512e8b
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-04-07 17:08:20 +0900

    mod: Post serializer.data에 comment 안보이는 오류 해결

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,8 +10,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.nickname')
-    comment = CommentSerializer(many=True, read_only=True)
+    comment = CommentSerializer(many=True, source='comments', read_only=True) #source=model의 related_name 명시해야 보임
 
     class Meta:
         model = Post
-        fields = ['id', 'user','content','is_anony','is_question','image','created_at','updated_at', 'comment']
+        fields = ['id', 'user','content','is_anony','is_question','image','created_at', 'updated_at','comment']
