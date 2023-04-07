7fe9fb0560ce8ffd8e23f5d939b60b0358478ccf
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-04-07 15:54:00 +0900

    del: 코드 정리 및 accounts/templates 삭제

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,13 +4,6 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    # path('home/', home, name='home'),
-    # path('signup/', signup, name='signup'),
-    # path('login/', login, name='login'),
-    # path('logout/', logout, name='logout'),
-    #
-    # path('friend/', friend, name='friend'),
-    # path('new-friend/<int:profile_id>/', new_friend, name='new-friend'),
     path('user/', UserList.as_view()),
     path('user/<int:user_id>/', UserDetail.as_view()),
     path('profile/', ProfileList.as_view()),
--- accounts/views.py
+++ accounts/views.py
@@ -9,47 +9,6 @@ from .serializers import ProfileSerializer, UserSerializer
 from rest_framework.views import APIView
 # Create your views here.
 
-
-# def signup(request):
-#     if request.method == "POST":
-#         if request.POST["password1"] == request.POST["password2"]:
-#             user = User.objects.create_user(
-#                 username=request.POST["username"],
-#                 password=request.POST["password1"])
-#             nickname = request.POST["nickname"]
-#             university = request.POST["university"]
-#             enter_year = request.POST["enter_year"]
-#             profile = Profile(user=user, nickname=nickname, university=university, enter_year=enter_year)
-#             profile.save()
-#             auth.login(request,user)
-#             return redirect('accounts:home')
-#     return render(request, 'accounts/signup.html')
-#
-#
-# def login(request):
-#
-#     if request.method == "POST":
-#         username=request.POST["username"]
-#         password=request.POST["password"]
-#         user = auth.authenticate(request, username=username, password=password)
-#         auth.login(request, user)
-#         if user is not None:
-#             return redirect('accounts:home')
-#         else:
-#             return render(request, 'accounts:login', {'error':'not correct'})
-#     else:
-#         return render(request, 'accounts/login.html')
-#
-#
-# def logout(request):
-#     auth.logout(request)
-#     return redirect('accounts:home')
-#
-#
-# def home(request):
-#     return render(request, 'accounts/home.html')
-#
-#
 # def friend(request):
 #     profiles = Profile.objects
 #     friends =Friend.objects
